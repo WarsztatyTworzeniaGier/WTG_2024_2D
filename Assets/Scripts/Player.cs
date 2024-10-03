7e9c842bf285b7c700e4328bea7b9957ab36b735
XardoX <45951614+XardoX@users.noreply.github.com>
2024-10-03 08:49:07 +0200

    init

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //To jest komentarz 
+    // Start is called before the first frame update
+    void Start()
+    {
+        //tu jest jakis kod i on co? robi
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Start();
+        Start();
+    }
+
+}
